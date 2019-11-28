78443751a6b772a63684136cbc60d8a1b090b9aa
Kyiml <mdt6150@rit.edu>
2019-11-28 04:20:57 -0500

    finally? fixed???

--- server/preinstall.js
+++ server/preinstall.js
@@ -10,16 +10,18 @@ const write_creds = () => {
     const GCP_vision_config_location = MODULE_path.resolve(
         `${__dirname}/../credentials/gcp_vision_auth.json`
     );
-    LMODULE_debug.print_message(`writing gcp auth to ${GCP_vision_config_location}`);
-    MODULE_fs.writeFile(
-        GCP_vision_config_location, GCP_vision_config,
-        (TMP_error_1) => {
-            if (TMP_error_1) {
-                LMODULE_debug.print_message(TMP_error_1);
-                return;
+    MODULE_fs.mkdir(MODULE_path.dirname(GCP_vision_config_location), { recursive: true }, () => {
+        LMODULE_debug.print_message(`writing gcp auth to ${GCP_vision_config_location}`);
+        MODULE_fs.writeFile(
+            GCP_vision_config_location, GCP_vision_config,
+            (TMP_error_1) => {
+                if (TMP_error_1) {
+                    LMODULE_debug.print_message(TMP_error_1);
+                    return;
+                }
             }
-        }
-    );
+        );
+    });
 };
 
 write_creds();
