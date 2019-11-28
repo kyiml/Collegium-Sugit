852ac2fadffa78607e59c25e0afc32fec720d4fe
Kyiml <mdt6150@rit.edu>
2019-11-28 04:08:29 -0500

    online build, attempt 6... gcp is annoying

--- server/preinstall.js
+++ server/preinstall.js
@@ -1,16 +1,19 @@
 const LMODULE_debug = require('./debug.js');
 const MODULE_fs = require('fs');
+const MODULE_path = require('path');
 
 const write_creds = () => {
     const GCP_vision_config = process.env.GCP_VISION_CONFIG;
     if (!GCP_vision_config) {
         return;
     }
+    const GCP_vision_config_location = `${dirname}/../credentials/gcp_vision_auth.json`;
+    LMODULE_debug.print_message(`writing gcp auth to ${GCP_vision_config_location}`);
     MODULE_fs.writeFile(
-        '/app/credentials/gcp_vision_auth.json', GCP_vision_config,
+        GCP_vision_config_location, GCP_vision_config,
         (TMP_error_1) => {
             if (TMP_error_1) {
-                LMODULE_debug.print_message('fatal error!');
+                LMODULE_debug.print_message(TMP_error_1);
                 return;
             }
         }
