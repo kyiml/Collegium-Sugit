9e6504e2ce81a2fe4189f7e1d15e5491d2682c7b
Kyiml <mdt6150@rit.edu>
2019-11-28 03:58:10 -0500

    online build, attempt 5 (hardcode attempt lol)

--- server/preinstall.js
+++ server/preinstall.js
@@ -6,10 +6,15 @@ const write_creds = () => {
     if (!GCP_vision_config) {
         return;
     }
-    MODULE_fs.writeFile('./credentials/gcp_vision_auth.json', GCP_vision_config, () => {
-        LMODULE_debug.print_message('fatal error!');
-        return;
-    });
+    MODULE_fs.writeFile(
+        '/app/credentials/gcp_vision_auth.json', GCP_vision_config,
+        (TMP_error_1) => {
+            if (TMP_error_1) {
+                LMODULE_debug.print_message('fatal error!');
+                return;
+            }
+        }
+    );
 };
 
 write_creds();
